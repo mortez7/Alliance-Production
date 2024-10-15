c76861f61f9ebf958448bdba21a069647caaae61
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-10-15 17:34:50 +0300

    add: header-features slider

--- js/main.js
+++ js/main.js
@@ -38,3 +38,31 @@ mMenuToggle.addEventListener("click", (event) => {
     event.preventDefault();
     menu.classList.contains("is-open") ? closeMenu() : openMenu();  
 });
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerView: 1,
+    autoHeight: true,
+    navigation: {
+        nextEl: ".slider-button-next",
+        prevEl: ".slider-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 5,
+          },
+      },
+  });
