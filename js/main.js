2016d887c250c0d8e5357db17cd651f9124fa024
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-10-16 17:27:09 +0300

    add: steps slider

--- js/main.js
+++ js/main.js
@@ -45,10 +45,9 @@ mMenuToggle.addEventListener("click", (event) => {
     menu.classList.contains("is-open") ? closeMenu() : openMenu();  
 });
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.features-slider', {
     speed: 400,
     slidesPerView: 1,
-    autoHeight: true,
     navigation: {
         nextEl: ".slider-button-next",
         prevEl: ".slider-button-prev",
@@ -70,5 +69,28 @@ const swiper = new Swiper('.swiper', {
         1200: {
             slidesPerView: 5,
           },
+      },
+  });
+
+  const swiperSteps = new Swiper(".steps-slider", {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".steps-button-next",
+        prevEl: ".steps-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,
+        },
       },
   });
