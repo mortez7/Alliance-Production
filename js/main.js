a4622ca3beb850a688b2bca9a9aedb9ac841389d
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-10-24 13:31:52 +0300

    add: blog section

--- js/main.js
+++ js/main.js
@@ -9,88 +9,106 @@ const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
 const lightModeOn = (event) => {
-    navbar.classList.add("navbar-light");
-    logo.style.display = "block";
-    logoLight.style.display = "none";
-    //logo.href.baseVal = "./img/sprite.svg#logo";
+  navbar.classList.add("navbar-light");
+  logo.style.display = "block";
+  logoLight.style.display = "none";
+  //logo.href.baseVal = "./img/sprite.svg#logo";
 };
 
 const lightModeOff = (event) => {
-    navbar.classList.remove("navbar-light");
-    logo.style.display = "none";
-    logoLight.style.display = "block";
-    //logo.href.baseVal = "./img/sprite.svg#logo-light";
+  navbar.classList.remove("navbar-light");
+  logo.style.display = "none";
+  logoLight.style.display = "block";
+  //logo.href.baseVal = "./img/sprite.svg#logo-light";
 };
 
 const openMenu = (event) => {
-    menu.classList.add("is-open");
-    mMenuToggle.classList.add("close-menu");
-    document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
-    lightModeOn();
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
+  lightModeOn();
 };
 
 const closeMenu = (event) => {
-    menu.classList.remove("is-open");
-    mMenuToggle.classList.remove("close-menu");
-    document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
-    lightModeOff();
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
+  lightModeOff();
 };
 
 window.addEventListener("scroll", () => {
-    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 
 mMenuToggle.addEventListener("click", (event) => {
-    event.preventDefault();
-    menu.classList.contains("is-open") ? closeMenu() : openMenu();  
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-const swiper = new Swiper('.features-slider', {
-    speed: 400,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: ".slider-button-next",
-        prevEl: ".slider-button-prev",
+const swiper = new Swiper(".features-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        // when window width is >= 320px
-        576: {
-          slidesPerView: 2,
-        },
-        // when window width is >= 768px
-        768: {
-          slidesPerView: 3,
-        },
-        // when window width is >= 1024px
-        1024: {
-          slidesPerView: 4,
-        },
-        // when window width is >= 1200px
-        1200: {
-            slidesPerView: 5,
-          },
-      },
-  });
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
 
-  const swiperSteps = new Swiper(".steps-slider", {
-    speed: 400,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: ".steps-button-next",
-        prevEl: ".steps-button-prev",
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        // when window width is >= 320px
-        576: {
-          slidesPerView: 2,
-        },
-        // when window width is >= 768px
-        768: {
-          slidesPerView: 3,
-        },
-        // when window width is >= 1024px
-        1024: {
-          slidesPerView: 4,
-        },
-      },
-  });
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiperBlog = new Swiper(".blog-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+    },
+    375: {
+      slidesPerView: 1,
+    },
+  },
+});
