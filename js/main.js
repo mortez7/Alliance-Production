fca093488bb7fa16e20a81cdfb08db76393e4ec2
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-10-29 12:50:49 +0200

    fix: modal script

--- js/main.js
+++ js/main.js
@@ -114,37 +114,22 @@ const swiperBlog = new Swiper(".blog-slider", {
 });
 
 const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal");
-const modalClose = document.querySelector(".modal-close");
+const modalDialog = document.querySelector(".modal-dialog");
 
-const openModal = () => {
-  modal.classList.add("is-open");
-};
-
-const closeModal = () => {
-  modal.classList.remove("is-open");
-};
-
-modalToggle.forEach((element) => {
-  element.addEventListener("click", (event) => {
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    openModal();
-  });
-});
-
-modalClose.addEventListener("click", (event) => {
-  event.preventDefault();
-  closeModal();
-});
-
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    closeModal();
+    modal.classList.toggle("is-open");
   }
 });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    closeModal();
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
   }
 });
